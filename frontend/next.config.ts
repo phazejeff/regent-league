23b4bb2cdf50a9f3796768dc33c649a1e7301bac
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-12-04 10:31:01 -0800

    oops im stupid

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,8 +17,8 @@ const nextConfig: NextConfig = {
       ]
   },
   env: {
-    // API_ROOT: 'https://regent-league-api.poopdealer.lol'
-    API_ROOT: 'http://127.0.0.1:8000'
+    API_ROOT: 'https://regent-league-api.poopdealer.lol'
+    // API_ROOT: 'http://127.0.0.1:8000'
   },
   images: {
     remotePatterns: [
