97394fc122587858d819db093c3af2237ba09d20
phazejeff <phazejeff@proton.me>
2026-04-05 18:48:39 -0700

    Updated placements script

--- api/input_placements.py
+++ api/input_placements.py
@@ -5,20 +5,21 @@ from sqlmodel import Session, delete, desc, select, func, or_, and_
 placements = {
     'Elites': [
         ('UBC Elite', 1, False),
-        ('CSUSM CS2 Blue', 2, False),
-        ('CSU Fullerton Blue', 3, True),
-        ('Gaucho Gaming', 3, True),
-        ('DU Crimson', 5, False),
-        ('Bronco Esports', 5, False)
+        ('CSU Fullerton Blue', 2, False),
+        ('Gaucho Gaming', 3, False),
+        ('Kansas State University', 4, False),
+        ('UCSC Onyx', 5, False),
+        ('Bronco Esports', 5, False),
+        ('CSUSM CS2 Blue', 5, False),
+        ('DU Crimson', 5, False)
     ],
     'Challengers' : [
-        ('University of Idaho Vandals', 1, False),
-        ('UCSC Sardonyx', 2, False),
-        ('Tritons Gold', 3, False),
-        ('CSUSM CS2 Black', 4, False),
+        ('NMSU Crimson', 1, False),
+        ('University of Idaho Vandals', 2, False),
+        ('UCSC Sardonyx', 3, False),
+        ('Tritons Gold', 4, False),
         ('CPP Indigo', 5, False),
-        ('UCSC Onyx', 5, False),
-        ('SU Esports & Gaming', 5, False)
+        ('SDSU Black', 5, False),
     ]
 }
 
@@ -32,8 +33,8 @@ for div in placements:
             team_id=team_db.id,
             placement=placement[1],
             division=div,
-            semester='Fall',
-            year=2025,
+            semester='Spring',
+            year=2026,
             split=placement[2]
         )
         session.add(p)
